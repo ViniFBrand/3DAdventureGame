9d5634faaf790dc6bcfa800a5d756cb4016c232c
vincenteamaga <vfbrandx6@gmail.com>
2025-10-06 16:28:28 -0300

    Integrated EnemyBase to Enemy_01
    Integração do script EnemyBase ao Enemy_01 e configuração inicial do Enemy_01 (adição de Prefab visual do inimigo e box collider)

--- Assets/Scripts/Enemies/EnemyBase.cs
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,11 @@ namespace Enemy
 
          
         [SerializeField] private float _currentLife;
+
+        [Header("Start Animation")]
+        public float startAnimationDuration = .2f;
+        public Ease startAnimationEase = Ease.OutBack;
+        public bool startWithBornAnimation = true;
 
         private void Awake()
         {
@@ -24,6 +30,8 @@ namespace Enemy
         protected virtual void Init()
         {
             ResetLife();
+            if(startWithBornAnimation)
+                BornAnimation();
         }
 
         protected virtual void Kill() 
@@ -44,6 +52,13 @@ namespace Enemy
                 Kill();
             }
         }
+
+        #region ANIMATION
+        private void BornAnimation()
+        {
+            transform.DOScale(0, startAnimationDuration).SetEase(startAnimationEase).From();
+        }
+        #endregion
 
         //debug
         private void Update()
