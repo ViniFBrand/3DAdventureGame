2c3a745a75cb03e611343b0a18dfcf8a378792d5
vincenteamaga <vfbrandx6@gmail.com>
2025-10-10 18:36:48 -0300

    More adjustements in Enemy_02 and configuration of feedback when enemy is hit
    Ajustes na movimentação do Enemy_02 (para observar o caminho que anda)
    Configuração do feedback de mover inimigo ao ser atingido por projetil

--- Assets/Scripts/Enemies/EnemyBase.cs
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -56,6 +56,8 @@ namespace Enemy
         {
             if (flashColor != null) flashColor.Flash();
             if (particleSystem != null) particleSystem.Emit(15);
+
+
             _currentLife -= f;
 
             if(_currentLife <= 0)
@@ -80,6 +82,12 @@ namespace Enemy
         {
             Debug.Log("Damage");
             OnDamage(damage);
+        }
+
+        public void Damage(float damage, Vector3 dir)
+        {
+            OnDamage(damage);
+            transform.DOMove(transform.position - dir, .1f);
         }
     }
 }
