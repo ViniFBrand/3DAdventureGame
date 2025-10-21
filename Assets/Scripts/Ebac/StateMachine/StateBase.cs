46407a1e7451e915792237f183e8ad59eced4c1f
vincenteamaga <vfbrandx6@gmail.com>
2025-10-21 14:23:02 -0300

    Started to implement Boss script
    Inicio da implementação do scrip de Boss com o BossBase e o BossStates (com já as classes BossStateBase, BossStateInit e BossStateWalk) e o registro dos estados no BossBase utilizando a StateMachine

--- Assets/Scripts/Ebac/StateMachine/StateBase.cs
+++ Assets/Scripts/Ebac/StateMachine/StateBase.cs
@@ -6,12 +6,12 @@ namespace Ebac.StateMachine
 { 
     public class StateBase 
     {
-        public virtual void OnStateEnter(object o = null)
+        public virtual void OnStateEnter(params object[] objs)
         {
             Debug.Log("OnStateEnter");
         }
         public virtual void OnStateStay()
-        {
+        {   
             Debug.Log("OnStateStay");
         }
         public virtual void OnStateExit()
